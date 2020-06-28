3bb6fb14765e70310b4b2110319198625cbd1393
Enrique Vargas <jevargas@seas.upenn.edu>
2020-06-27 19:33:26 -0500

    add initial interface for tree

---
+++ huffmantree.h
@@ -0,0 +1,29 @@
+#ifndef __HUFFMANTREE_H__
+#define __HUFFMANTREE_H__
+
+struct treenode {
+        int nbits;
+        int n;
+        char character;
+        struct treenode *zero;
+        struct treenode *one;
+};
+
+/* given an array of ints representing frequencies indexed by ascii code
+ * build a huffman tree returning its root
+ */ 
+struct treenode* build_tree(int *frequencies);
+
+/* add a bit and return next node in the tree, if its a leaf
+ * zero and one pointers will be null and nbits/n/character
+ *  contain relevant values
+ */ 
+struct treenode* decode_bit(struct treenode *t, unsigned short bit);
+
+/* given a char and a huffman tree return a leaf node with 
+ * its proper encoding
+ */ 
+struct treenode* encode_char(struct treenode *root, char c);
+
+
+#endif
