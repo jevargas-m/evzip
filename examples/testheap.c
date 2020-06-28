032de6985c29bf5e47a632674e9928a08326742f
Enrique Vargas <jevargas@seas.upenn.edu>
2020-06-28 09:57:26 -0500

    heap working

--- examples/testheap.c
+++ examples/testheap.c
@@ -6,7 +6,7 @@ int cmp(void* e1, void* e2) {
         int* e1_int = (int*) e1;
         int* e2_int = (int*) e2;
 
-        return *e1_int - *e2_int;
+        return *e2_int - *e1_int;
 }
 
 int main () {
@@ -31,12 +31,18 @@ int main () {
         }
         printf("\n");      
 
-        /*
-        printf("min = ");
-        int *e_ptr = (int*) remove_min(&my_heap);
-        printf("%d ", *e_ptr);
-        printf("\n");      
-        */
+        printf("heapSort = ");
+        for (int i = 0; i < n; i++) {
+                int *e_ptr = (int*) remove_min(&my_heap);
+                printf("%d ", *e_ptr);
+        }
+        printf("\n");     
+        
+        printf("input = ");
+        for (int i = 0; i < n; i++) {
+                printf("%d ", my_ints[i]);
+        }
+        printf("\n");  
 
         destroy_heap(&my_heap);
 
